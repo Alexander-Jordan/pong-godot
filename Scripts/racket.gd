f7ca9d3c8fe1e58b0a6b6dbee3d2aa51a74c52b6
Alexander Jordan <curtjordan93@gmail.com>
2023-07-11 12:09:53 +0200

    Move the racket

--- Scripts/racket.gd
+++ Scripts/racket.gd
@@ -1,11 +1,16 @@
-extends Node
+extends CharacterBody2D
 
+@export var speed = 400
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func get_input():
+	var direction = Input.get_axis("up-p1", "down-p1")
+	velocity = Vector2(0, direction * speed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
