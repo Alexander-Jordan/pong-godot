8855e292216433e3f6f612a24e6cd1702d7d4958
Alexander Jordan <curtjordan93@gmail.com>
2023-07-11 12:18:54 +0200

    2 players

--- Scripts/racket.gd
+++ Scripts/racket.gd
@@ -1,13 +1,15 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@export var upAction := "up"
+@export var downAction := "down"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func get_input():
-	var direction = Input.get_axis("up-p1", "down-p1")
+	var direction = Input.get_axis(upAction, downAction)
 	velocity = Vector2(0, direction * speed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
