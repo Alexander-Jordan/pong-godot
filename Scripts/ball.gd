09dc5c2aec19ccd6e9201e549c4368a9caec63b0
Alexander Jordan <curtjordan93@gmail.com>
2023-07-17 11:25:05 +0200

    Use godot physics for ball, add walls and adjust code

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends CharacterBody2D
 
 var screen_size:Vector2
 var rng = RandomNumberGenerator.new()
@@ -12,26 +12,16 @@ func _ready():
 	set_random_direction()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if direction.length() > 0:
-		direction = direction.normalized() * speed
-	
-	if position.y < 0 || position.y > screen_size.y:
-		bounce_off_wall()
-	
-	position += direction * delta
+func _physics_process(delta):
+	var collision = move_and_collide(velocity * speed * delta)
+	if collision:
+		velocity = velocity.bounce(collision.get_normal())
 
 func set_random_direction():
 	var zero_or_one = rng.randi_range(0, 1)
 	direction = Vector2(-1 if zero_or_one == 0 else 1, rng.randf_range(-1, 1))
-
-func bounce_off_wall():
-	direction.y = -direction.y if sign(direction.y) == 1 else abs(direction.y)
+	velocity = direction.normalized()
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	position = Vector2(screen_size.x/2, screen_size.y/2)
 	set_random_direction()
-
-func _on_body_entered(body):
-	if body.has_method("set_new_ball_direction"):
-		direction = body.set_new_ball_direction(position, direction)
--- Scripts/racket.gd
+++ Scripts/racket.gd
@@ -1,16 +1,8 @@
 extends CharacterBody2D
 
-# temp variable used to prevent out of bounds
-var screenSize:Vector2
-
 @export var speed:int = 400
 @export var upAction:String = "up"
 @export var downAction:String = "down"
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	# get screen size to prevent racket to go out of bounds
-	screenSize = get_viewport_rect().size
 
 func get_input():
 	var direction = Input.get_axis(upAction, downAction)
@@ -19,11 +11,9 @@ func get_input():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	get_input()
-	move_and_slide()
-	# simple temp constraint to prevent racket to move out of bounds
-	position.y = clamp(position.y, 0, screenSize.y)
+	move_and_collide(velocity * delta)
 
-func set_new_ball_direction(current_position:Vector2, current_direction:Vector2) -> Vector2:
-	# set new direction by flipping x direction
-	var new_x_direction:float = -current_direction.x if sign(current_direction.x) == 1 else abs(current_direction.x)
-	return Vector2(new_x_direction, current_direction.y)
+func ball_velocity_after_bounce(ball_velocity:Vector2) -> Vector2:
+	# set new direction by flipping x
+	var new_x_direction:float = -ball_velocity.x if sign(ball_velocity.x) == 1 else abs(ball_velocity.x)
+	return Vector2(new_x_direction, ball_velocity.y)
