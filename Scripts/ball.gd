8d0c680699c536f24c933950eb38f754676a38b4
Alexander Jordan <curtjordan93@gmail.com>
2023-07-18 09:59:20 +0200

    Add modular speed depending on where it hit the racket

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -3,11 +3,14 @@ extends CharacterBody2D
 var screen_size:Vector2
 var rng = RandomNumberGenerator.new()
 var direction:Vector2 = Vector2.ZERO
+var speed:int = 400
 
-@export var speed:int = 400
+@export var min_speed:int = 400
+@export var max_speed:int = 600
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	speed = min_speed
 	screen_size = get_viewport_rect().size
 	set_random_direction()
 
@@ -16,6 +19,8 @@ func _physics_process(delta):
 	var collision = move_and_collide(velocity * speed * delta)
 	if collision:
 		var collider:Object = collision.get_collider()
+		if collider.has_method("ball_speed_after_bounce"):
+			speed = collider.ball_speed_after_bounce(min_speed, max_speed, position)
 		if collider.has_method("ball_velocity_after_bounce"):
 			velocity = collider.ball_velocity_after_bounce(velocity, position)
 		else:
@@ -28,4 +33,5 @@ func set_random_direction():
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	position = Vector2(screen_size.x/2, screen_size.y/2)
+	speed = min_speed
 	set_random_direction()
--- Scripts/racket.gd
+++ Scripts/racket.gd
@@ -10,7 +10,6 @@ var shape:RectangleShape2D
 func _ready():
 	shape = $CollisionShape2D.shape
 	size = shape.size
-	print(size)
 
 func get_input():
 	var direction = Input.get_axis(upAction, downAction)
@@ -20,6 +19,19 @@ func get_input():
 func _physics_process(delta):
 	get_input()
 	move_and_collide(velocity * delta)
+
+func ball_speed_after_bounce(min_speed:float, max_speed:float, ball_position:Vector2) -> float:
+	# how far from the center in y-axis was the hit?
+	var from_center:float = ball_position.y - position.y
+	# normalizing from_center (20 to -20 range -> 1 to -1 range)
+	# that's done by dividing from_center with max value (in this case: height of collider / 2)
+	var from_center_normalized:float = from_center / (size.y / 2)
+	# convert from_center_normalized to speed_multiplyer by converting value to absolute (force positive)
+	var speed_multiplyer = abs(from_center_normalized)
+	# get the speed range
+	var speed_range = max_speed - min_speed
+	# min_speed is always included, and then add the speed_range multiplied with the multiplyer
+	return min_speed + (speed_range * speed_multiplyer)
 
 func ball_velocity_after_bounce(ball_velocity:Vector2, ball_position:Vector2) -> Vector2:
 	# new x is simply x flipped
