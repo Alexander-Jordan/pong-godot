e5bf4517d82605485e8692722d0caf35808a7374
Alexander Jordan <curtjordan93@gmail.com>
2023-08-07 10:04:08 +0200

    Clean up GameUI

--- Scripts/game_ui.gd
+++ Scripts/game_ui.gd
@@ -1,16 +1,16 @@
 extends Control
 
 func _on_game_manager_game_reset():
-	$PlayerOneScore.text = "0"
-	$PlayerTwoScore.text = "0"
+	$Score/PlayerOne.text = "0"
+	$Score/PlayerTwo.text = "0"
 	$GameOver.visible = false
 
 
 func _on_game_manager_point_change(target:String, new_total_points:int):
 	if target == "player_one":
-		$PlayerOneScore.text = str(new_total_points)
+		$Score/PlayerOne.text = str(new_total_points)
 	elif target == "player_two":
-		$PlayerTwoScore.text = str(new_total_points)
+		$Score/PlayerTwo.text = str(new_total_points)
 
 
 func _on_game_manager_game_over():
