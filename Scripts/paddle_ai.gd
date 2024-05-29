44d1d27ce36cf7699c810566ddaed0772556daa6
Alexander Jordan <curtjordan93@gmail.com>
2024-05-29 03:58:04 +0200

    Add some kind of diffferent ai difficulty

--- Scripts/paddle_ai.gd
+++ Scripts/paddle_ai.gd
@@ -1,29 +1,30 @@
 extends Node2D
 class_name PaddleAI
 
+var center:Vector2
 var side:String
 var ball_position:Vector2 = Vector2.ZERO
 var ball_velocity:Vector2 = Vector2.ZERO
 
 func _ready():
 	var screen_size = get_viewport_rect().size
-	var center:Vector2 = Vector2(screen_size.x/2, screen_size.y/2)
-	side = 'left' if position.x < center.x else 'right'
+	center = Vector2(screen_size.x/2, screen_size.y/2)
+	side = 'left' if global_position.x < center.x else 'right'
 
 func get_direction(AI:int) -> int:
 	if AI == 1:
 		return easy_ai()
 	if AI == 2:
-		return hard_ai()
+		return normal_ai()
 	if AI == 3:
-		return impossible_ai()
+		return hard_ai()
 	return 0
 
 func easy_ai() -> int:
 	# ignore the ball if it is going away from the paddle
-	if side == 'left' and ball_velocity.x < 0:
+	if side == 'left' and ball_velocity.x > 0:
 		return 0
-	if side == 'right' and ball_velocity.x > 0:
+	if side == 'right' and ball_velocity.x < 0:
 		return 0
 	
 	if global_position.y > ball_position.y:
@@ -33,8 +34,34 @@ func easy_ai() -> int:
 	
 	return 0
 
-func hard_ai():
+func normal_ai():
+	# ignore the ball if it is going away from the paddle
+	if side == 'left' and ball_velocity.x > 0:
+		if global_position.y > center.y:
+			return -1
+		elif global_position.y < center.y:
+			return 1
+		else:
+			return 0
+	if side == 'right' and ball_velocity.x < 0:
+		if global_position.y > center.y:
+			return -1
+		elif global_position.y < center.y:
+			return 1
+		else:
+			return 0
+	
+	if global_position.y > ball_position.y:
+		return -1
+	if global_position.y < ball_position.y:
+		return 1
+	
 	return 0
 
-func impossible_ai():
+func hard_ai():
+	if global_position.y > ball_position.y:
+		return -1
+	if global_position.y < ball_position.y:
+		return 1
+	
 	return 0
